d0fe26820daf7999538a406e69373cc7049af4ac
Ique <iquecode@gmail.com>
2022-03-15 10:37:58 -0300

    start repository - basci layout

---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/template/Layout'
+
+export default function Notificacoes() {
+  return (
+    <Layout titulo='Notificações' subtitulo='Aqui vocÊ irá gerenciar as suas nottificações'>
+      <h3>Conteúdo!!!</h3>   
+    </Layout>
+
+  )
+}
