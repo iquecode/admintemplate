394065ab4dc41814f8d7ba541168359a4ea2a3a2
ique <iquecode@gmail.com>
2022-03-16 16:53:12 -0300

    continue layout

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,15 +1,10 @@
 import Layout from '../components/template/Layout'
-import useAppData from '../data/hook/useAppData'
 
 export default function Notificacoes() {
-
-  const { alternarTema } = useAppData()
-
   return (
-    <Layout titulo='Notificações' subtitulo='Aqui vocÊ irá gerenciar as suas nottificações'>
-      <h3>Conteúdo!!!</h3> 
-      <button onClick={alternarTema}>Alternar Tema</button>
+    <Layout titulo='Notificações' 
+      subtitulo='Aqui você irá gerenciar as suas nottificações'>
+      <h1>Notificações</h1> 
     </Layout>
-
   )
 }
